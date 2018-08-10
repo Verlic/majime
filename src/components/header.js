de31d05d6ff679d931b3b0747a1fd80f45fc58c2
Hernan Meydac <hernan.meydac@auth0.com>
2018-08-10 19:42:28 -0300

    First commit

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header id="main-header">
+                <div>Majime - まじめ</div>
+            </header>
+        );
+    }
+}
+
+export default Header;
+
+
